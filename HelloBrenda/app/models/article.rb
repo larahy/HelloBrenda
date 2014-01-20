666daf38e1a63eeb016af5a7f2ad558e1524600b
larahy <larahy@gmail.com>
2014-01-20 17:07:14 +0000

    AWS cloud services

--- HelloBrenda/app/models/article.rb
+++ HelloBrenda/app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ActiveRecord::Base
   has_and_belongs_to_many :tags
-  has_attached_file :image, styles: { thumb: "300x300>" }
+  has_attached_file :image, 
+                    styles: { thumb: "300x300>" }, 
+                    storage: :s3, 
+                    s3_credentials: {
+                      access_key_id: 'AKIAJDSSWRQYZB7YML4Q',
+                      secret_access_key: Rails.application.secrets.secret_access_key 
+                      },
+                    bucket: 'HelloBrendaPhotos'
 end
